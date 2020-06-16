13884a0b6b1cc22170835c68f4f56a0ff4326894
eunjin <ejchoi.infinyx@gmail.com>
2020-06-17 00:50:34 +0900

    Customized user add, but I donno why it works

--- MEgo/admin.py
+++ MEgo/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import Event
+from django.contrib.auth.admin import UserAdmin
+from .models import Experience
+from .models import User
 
-admin.site.register(Event)
+admin.site.register(Experience)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'nickname',
+        'email',
+        'date_joined',
+    )
+
+    list_display_links = (
+        'nickname',
+        'email',
+    )
--- MEgo/models.py
+++ MEgo/models.py
@@ -1,9 +1,60 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
 
 
-class Event(models.Model):
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, nickname, password=None):
+        if not email:
+            raise ValueError('must have user email')
+        user = self.model(
+            email=self.normalize_email(email),
+            nickname=nickname
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, nickname, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            nickname=nickname,
+            password=password
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+    nickname = models.CharField(
+        max_length=20,
+        null=False,
+        unique=True
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = 'nickname'
+    REQUIRED_FIELDS = ['email']
+
+class Experience(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='admin')
     input_date = models.DateTimeField(
             default=timezone.now)
@@ -17,7 +68,7 @@ class Event(models.Model):
     calorie = models.IntegerField(blank=True, null=True)
     distance = models.FloatField(blank=True, null=True)
     # Mental Data
-    event = models.TextField()
+    Experience = models.TextField()
     thoughts = models.TextField(blank=True, null=True)
     emotion = models.TextField()
     emotion_intensity = models.IntegerField()
@@ -30,3 +81,6 @@ class Event(models.Model):
     def publish(self):
         self.exp_date = timezone.now()
         self.save()
+
+    def __str__(self):
+        return self.event
