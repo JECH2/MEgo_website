48b6a7217b35fd8dcadcb5874e478e2edccfbc8f
eunjin <ejchoi.infinyx@gmail.com>
2020-07-02 02:52:06 +0900

    data synch

--- export_data.py
+++ export_data.py
@@ -8,15 +8,30 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MEgo_website.settings")
 django.setup()
 
 # 어떤 모델에서 가져올 것인지
-from MEgo.models import Experience, User
+#from MEgo.models import Experience, User
 
-with open('exp.csv', 'w', newline='') as csvfile:
-    fieldnames = ['emotion', 'event'] # 가져올 field name
+#with open('exp.csv', 'w', newline='') as csvfile:
+#    fieldnames = ['emotion', 'event'] # 가져올 field name
+#    data_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+#    data_writer.writeheader()
+
+#    usr = User.objects.get(user_id='jech') # 누구의 데이터를 가져올 것인가
+
+#    for exp in Experience.objects.filter(author__exact=usr.id): # 필터링해서 가져올 수도 있다.
+#        data_writer.writerow({'emotion':exp.emotion, 'event':exp.event})
+
+from MEgo.models import *
+CSV_PATH = 'MEgo/report/jiseong.csv'
+username = os.path.basename(CSV_PATH)[:-4]
+
+with open(CSV_PATH, 'w', newline='') as csvfile:
+    fieldnames = ['author','emotion_label', 'event', 'thoughts','importance','related_people','related_place'] # 가져올 field name
     data_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     data_writer.writeheader()
 
-    usr = User.objects.get(user_id='jech') # 누구의 데이터를 가져올 것인가
+    usr = User.objects.get(user_id=username)
 
     for exp in Experience.objects.filter(author__exact=usr.id): # 필터링해서 가져올 수도 있다.
-        data_writer.writerow({'emotion':exp.emotion, 'event':exp.event})
+        data_writer.writerow({'author':exp.author,'emotion_label':exp.emotion, 'event':exp.event,'thoughts':exp.thoughts, 'related_people':exp.related_people, 'related_place':exp.related_place})
--- import_data.py
+++ import_data.py
@@ -7,18 +7,18 @@ django.setup()
 
 from MEgo.models import ExpQuestions, LifeQuestions, EmotionColor
 
-CSV_PATH = 'Question Data - Daily Questions.csv'
+#CSV_PATH = 'Question Data - Daily Questions.csv'
 
-with open(CSV_PATH, newline='') as csvfile:
-   data_reader = csv.DictReader(csvfile)
-   for row in data_reader:
-       print(row)
-       ExpQuestions.objects.create(
-           content = row['content'],
-           question_area = row['question area'],
-           related_tags = row['related tags'],
-           answer_area = row['answer area']
-       )
+#with open(CSV_PATH, newline='') as csvfile:
+#   data_reader = csv.DictReader(csvfile)
+#   for row in data_reader:
+#       print(row)
+#       ExpQuestions.objects.create(
+#           content = row['content'],
+#           question_area = row['question area'],
+#           related_tags = row['related tags'],
+#           answer_area = row['answer area']
+#       )
 
 #
 # CSV_PATH = 'Question Data - Life Questions.csv'
@@ -47,3 +47,24 @@ with open(CSV_PATH, newline='') as csvfile:
 #            a = row['a']
 #        )
 #
+from MEgo.models import Experience, User
+from MEgo.color import emo_to_hex
+CSV_PATH = 'MEgo/report/jiseong.csv'
+username = os.path.basename(CSV_PATH)[:-4]
+usr = User.objects.get(user_id=username)
+with open(CSV_PATH, newline='') as csvfile:
+    data_reader = csv.DictReader(csvfile)
+
+    for row in data_reader:
+        print(row)
+        parsed_emotion = row['emotion_label'].strip().split(',')
+        exp = Experience.objects.create(
+            author = usr,
+            event = row['event'],
+           thoughts = row['thoughts'],
+          emotion = row['emotion_label'],
+            importance = row['importance'],
+            related_people=row['related_people'],
+            related_place=row['related_place'],
+            emotion_color=emo_to_hex(parsed_emotion),
+        )
